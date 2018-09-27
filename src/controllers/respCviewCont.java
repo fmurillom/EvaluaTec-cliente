d1037483209e87624cca7aa614ee3f22b6c226f8
Francisco Murillo <fmurillom@gmail.com>
2018-09-26 23:01:39 -0600

    Arregaldo el sdk

--- src/controllers/respCviewCont.java
+++ src/controllers/respCviewCont.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import javafx.collections.FXCollections;
+import javafx.collections|.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
